d95991fdd233d69a49afae0dc340e4417fefff53
Brian <mwakabrian00@gmail.com>
2023-10-27 22:22:14 +0300

    Define parameters

---
+++ main.py
@@ -0,0 +1,13 @@
+# Morgage Calculator
+
+# Using the Loan Amortization formula below,
+
+# M = P[i(1+i)^n]/(1+i)^n - 1]
+
+# Where:
+# M = Total monthly payment -7452.81
+# P = Total principal loan amount - $100,000
+# i = % monthly interest rate -7.5% (0.075 as a decimal)
+# n = number of payments (loan duration in months)-30 years, so 30 * 12 = 360 months
+
+# Defining parameters in Calculating Monthly payments
