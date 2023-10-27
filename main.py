4ed24e6d1151a989d5147c17d2d7e2288040186e
Brian <mwakabrian00@gmail.com>
2023-10-27 22:25:20 +0300

    Calculate monthly payments

--- main.py
+++ main.py
@@ -11,3 +11,21 @@
 # n = number of payments (loan duration in months)-30 years, so 30 * 12 = 360 months
 
 # Defining parameters in Calculating Monthly payments
+def calculate_monthly_payment(principal, annual_interest_rate, years):
+    monthly_interest_rate = annual_interest_rate / 100 / 12
+    total_months = years * 12
+    monthly_payment = (principal * monthly_interest_rate) / (1 - (1 + monthly_interest_rate)**-total_months)
+    return monthly_payment
+
+def main():
+    principal = float(input("Enter the loan principal: "))
+    annual_interest_rate = float(input("Enter the annual interest rate (in percentage): "))
+    years = int(input("Enter the number of years: "))
+    
+    monthly_payment = calculate_monthly_payment(principal, annual_interest_rate, years)
+    
+    print(f"Result...\nFor a {years}-Year mortgage loan of ${principal:.2f} at an annual interest rate of {annual_interest_rate:.2f}% you pay ${monthly_payment:.2f} Monthly")
+    print(f"Total amount paid will be ${monthly_payment * years * 12:.2f}")
+
+if __name__ == "__main__":
+    main()
